14116cd6fe8f8933c3feb50d622e51abdae43c7b
StephenHolmes14 <Stephenholmes1995@googlemail.com>
2022-02-13 14:45:25 +0000

    Project Euler 20

--- src/challenges/project_euler_20.rs
+++ src/challenges/project_euler_20.rs
@@ -12,7 +12,37 @@ pub fn project_euler_20() {
     let start = Instant::now();
     let mut answer = 0;
 
-    let one_hundred_factorial: u64 = (1..101).reduce(|x,y| x * y).unwrap();
+    // Adjusted from 16 algo
+    let mut digits: Vec<i32> = Vec::new();
+
+    digits.push(1);
+
+    // This can go way above 10 now
+    let mut current_carry_over = 0;
+
+    for n in 1..101 {
+        current_carry_over = 0;
+        for digit_index in 0..digits.len() {
+            let mut new_digit = digits.get(digit_index).unwrap() * n;
+
+            let new_digit_adjusted = (current_carry_over + new_digit) % 10;
+
+            digits[digit_index] = new_digit_adjusted;
+
+            current_carry_over = (current_carry_over + new_digit - new_digit_adjusted)/10;
+
+            if digit_index == digits.len()-1 && current_carry_over > 0 {
+                // Current carry over digits in reverse order
+                current_carry_over.to_string()
+                    .chars()
+                    .rev()
+                    .map(|c| i32::from_str_radix(&c.to_string(), 10).unwrap())
+                    .for_each(|x| digits.push(x));
+            }
+        }
+    }
+
+    answer = digits.into_iter().reduce(|x,y| x + y).unwrap();
 
     println!("Project Euler 20: {}, Time Taken: {}", answer, start.elapsed().as_secs());
 }
