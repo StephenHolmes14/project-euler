83408ae5a4ae1f2f6a3f7db144304e8786f9f1f9
StephenHolmes14 <Stephenholmes1995@googlemail.com>
2022-04-15 16:13:15 +0100

    Project Euler 27

---
+++ src/challenges/project_euler_27/solution.rs
@@ -0,0 +1,71 @@
+use std::borrow::Borrow;
+use std::time::Instant;
+
+#[path = "../../utils/prime_utils.rs"] mod prime_utils;
+
+use prime_utils::is_prime_i64;
+
+/**
+Euler discovered the remarkable quadratic formula:
+
+
+It turns out that the formula will produce 40 primes for the consecutive integer values . However, when  is divisible by 41, and certainly when  is clearly divisible by 41.
+
+The incredible formula  was discovered, which produces 80 primes for the consecutive values . The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+, where  and
+
+where  is the modulus/absolute value of
+e.g.  and
+Find the product of the coefficients,  and , for the quadratic expression that produces the maximum number of primes for consecutive values of , starting with .
+ */
+pub fn calculate() {
+    let start = Instant::now();
+    let mut answer = 0;
+    let mut answer_count = 0;
+
+    // brute force
+
+    for a in i64::from(-999)..1000 {
+        if !is_prime_i64(a.abs()) {
+            continue;
+        }
+
+
+        for b in i64::from(-1000)..1001 {
+            if !is_prime_i64(b.abs()) {
+                continue;
+            }
+
+            println!("Found Prime b: {}", b);
+
+            let count = count_primes(a, b);
+
+            if count > answer_count {
+                answer = a * b;
+                answer_count = count;
+            }
+        }
+    }
+
+    println!("Project Euler 26: {}, Time Taken: {}", answer, start.elapsed().as_secs());
+}
+
+fn count_primes(a: i64, b: i64) -> i64 {
+    let mut only_primes_found = true;
+    let mut n = 1;
+
+    while only_primes_found {
+        let value = i64::pow(n,2) + a*n + b;
+
+        if !is_prime_i64(value.abs()) {
+            break;
+        }
+
+        n = n + 1;
+    }
+
+    return n;
+}
---
+++ src/utils/prime_utils.rs
@@ -0,0 +1,31 @@
+pub fn is_prime_i64(value: i64) -> bool {
+    if value == 1 {
+        return true;
+    }
+
+    if value == 0 {
+        return false;
+    }
+
+    let sqrt = (value as f64).sqrt() as i64;
+
+    for possible_factor in 2..(sqrt+1) {
+        if value % possible_factor == 0 {
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn is_prime_i32(value: i32) -> bool {
+    let sqrt = (value as f64).sqrt() as i32;
+
+    for possible_factor in 2..(sqrt+1) {
+        if value % possible_factor == 0 {
+            return false;
+        }
+    }
+
+    true
+}
