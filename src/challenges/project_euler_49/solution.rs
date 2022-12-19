800597e5090e7bb7200bcfcfa959be21657db6a5
StephenHolmes14 <Stephenholmes1995@googlemail.com>
2022-12-19 21:59:46 +0000

    Project Euler 49

--- src/challenges/project_euler_49/solution.rs
+++ src/challenges/project_euler_49/solution.rs
@@ -2,16 +2,18 @@ use std::ops::{Add, Div, Sub};
 use std::time::Instant;
 use std::collections::{BTreeSet, HashSet};
 
-#[path = "../../utils/vector_maths_utils.rs"] mod vector_maths_utils;
+#[path = "../../utils/prime_utils.rs"] mod prime_utils;
+#[path = "../../utils/string_utils.rs"] mod string_utils;
 
-use vector_maths_utils::add;
-use vector_maths_utils::power_i32;
-use vector_maths_utils::convert_vec_to_u64;
+use prime_utils::is_prime_i32;
+use string_utils::sort;
 
 ///
-/// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+/// The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
 //
-// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+// There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
+//
+// What 12-digit number do you form by concatenating the three terms in this sequence?
 //
 ///
 
@@ -25,18 +27,40 @@ pub fn calculate() {
 }
 
 pub fn find_answer() -> u64 {
-    // Use vectors to track the digits
-    let mut sum_vec = Vec::from([0]);
+    let mut current = 1000;
+    let mut primes: BTreeSet<i32> = BTreeSet::new();
 
-    for i in 1..1001 {
-        let i_sq_vec = power_i32(i, i);
-        sum_vec = add(&sum_vec, &i_sq_vec);
+    loop {
+        current += 1;
 
-        // println!("i: {}, i_sq_vec: {:?}, sum_vec: {:?}", i, i_sq_vec, sum_vec);
+        if !is_prime_i32(current) {
+            continue;
+        }
 
+        if current == 8147 {
+            continue;
+        }
+
+        for prime in primes.iter().rev() {
+            let diff = current - prime;
+            let smallest_prime = prime - diff;
+
+            if primes.contains(&smallest_prime) {
+                let smallest_string = smallest_prime.to_string();
+                let middle_string = prime.to_string();
+                let biggest_string = current.to_string();
+
+                let smallest_sorted = sort(&smallest_string);
+                let middle_sorted = sort(&middle_string);
+                let biggest_sorted = sort(&biggest_string);
+
+                if smallest_sorted.eq(&middle_sorted) && middle_sorted.eq(&biggest_sorted) {
+                    let number_string = smallest_string + &middle_string + &biggest_string;
+                    return u64::from_str_radix(&number_string, 10).unwrap();
+                }
+            }
+        }
+
+        primes.insert(current);
     }
-
-    let last_10_numbers: Vec<i32> = sum_vec.into_iter().take(10).collect();
-
-    convert_vec_to_u64(&last_10_numbers)
 }
