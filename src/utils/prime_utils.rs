e61e46e93a4e54140e8fafc07ece92be0f6bcb19
StephenHolmes14 <Stephenholmes1995@googlemail.com>
2022-12-28 09:52:30 +0000

    Implement more efficient is prime algo

--- src/utils/prime_utils.rs
+++ src/utils/prime_utils.rs
@@ -1,43 +1,57 @@
 pub fn is_prime_i64(value: i64) -> bool {
-    if value == 1 {
+    if value == 2 || value == 3 {
+        return true;
+    }
+
+    if value <= 1 || value % 2 == 0 || value % 3 == 0 {
         return false;
     }
 
-    if value == 0 {
-        return false;
-    }
+    let mut i = 5;
 
-    let sqrt = (value as f64).sqrt() as i64;
+    loop {
+        // Primes come in pairs, one less than the sqrt and one larger (or both equal)
+        // If we reach a value of i larger than sqrt of the number, we must have a prime number
+        if i * i > value {
+            return true;
+        }
 
-    for possible_factor in 2..(sqrt+1) {
-        if value % possible_factor == 0 {
+        // If the current i is a factor of the value, it's not prime
+        // We also check the current i + 2 in this algorithm
+        if value % i == 0 || value % (i + 2) == 0 {
             return false;
         }
-    }
 
-    true
+        // We only need to check every 6 numbers
+        i += 6;
+    }
 }
 
 pub fn is_prime_i32(value: i32) -> bool {
-    if value == 1 {
-        return false;
-    }
-    
-    if value == 2 {
+    if value == 2 || value == 3 {
         return true;
     }
     
-    if value % 2 == 0 {
+    if value <= 1 || value % 2 == 0 || value % 3 == 0 {
         return false;
     }
     
-    let sqrt = (value as f64).sqrt() as i32;
+    let mut i = 5;
 
-    for possible_factor in 2..(sqrt+1) {
-        if value % possible_factor == 0 {
+    loop {
+        // Primes come in pairs, one less than the sqrt and one larger (or both equal)
+        // If we reach a value of i larger than sqrt of the number, we must have a prime number
+        if i * i > value {
+            return true;
+        }
+
+        // If the current i is a factor of the value, it's not prime
+        // We also check the current i + 2 in this algorithm
+        if value % i == 0 || value % (i + 2) == 0 {
             return false;
         }
-    }
 
-    true
+        // We only need to check every 6 numbers
+        i += 6;
+    }
 }
